a940c97f692aaae7a3fad06b37cb58cf6c59d9ea
David Chang <chechiachang999@gmail.com>
2019-10-25 16:02:34 +0800

    Add PPToken

---
+++ ethereum/contracts/PPToken.sol
@@ -0,0 +1,70 @@
+/**
+ *Submitted for verification at Etherscan.io on 2019-10-03
+*/
+
+pragma solidity ^0.4.22;
+
+contract MyToken {
+    string public name = 'Party Parrot Token';
+    string public symbol = 'PPT';
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    // approve
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor (uint256 _innitialSupply) public {
+        balanceOf[msg.sender] = _innitialSupply;
+        totalSupply = _innitialSupply;
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    // approve
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+}
